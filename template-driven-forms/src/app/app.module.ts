1286502e9c46925892526636bb312c8aa88cb67a
sagar.ranganath <sagar.ranganath@tu-ilmenau.de>
2017-12-23 20:30:21 +0100

    Added Github followers component

--- template-driven-forms/src/app/app.module.ts
+++ template-driven-forms/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { PostsComponent } from './posts/posts.component';
 import {HttpModule} from '@angular/http';
 import {PostService} from './services/post.service';
 import {AppErrorHandler} from './Common/app-error-handler';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import {GithubFollowersService} from './services/github-followers.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import {AppErrorHandler} from './Common/app-error-handler';
     SignupFormComponent,
     NewCourseFormComponent,
     ChangePasswordComponent,
-    PostsComponent
+    PostsComponent,
+    GithubFollowersComponent
   ],
   imports: [
     BrowserModule,
@@ -29,6 +32,7 @@ import {AppErrorHandler} from './Common/app-error-handler';
   ],
   providers: [
     PostService,
+    GithubFollowersService,
     {provide: ErrorHandler, useClass: AppErrorHandler}
   ],
   bootstrap: [AppComponent]
